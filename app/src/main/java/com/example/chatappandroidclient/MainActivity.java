5cd73e379c8f5429f45374e74f246a13d3b91ec9
matanshamir1998 <matan.shamir@live.biu.ac.il>
2022-06-02 12:19:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chatappandroidclient/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.chatappandroidclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            Intent i = new Intent(this, Register.class);
+            startActivity(i);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/chatappandroidclient/Register.java
@@ -0,0 +1,24 @@
+package com.example.chatappandroidclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(v -> {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        });
+
+    }
+}
